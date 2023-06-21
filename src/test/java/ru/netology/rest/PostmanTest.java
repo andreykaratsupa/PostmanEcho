9f1fcdbec3f8339106f868008bcd1473ac0e836d
Karatsupa <andreykaratsupa@gmail.com>
2023-06-21 22:12:55 +0300

    First

---
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void ShouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
