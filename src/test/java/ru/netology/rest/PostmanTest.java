7e98b4ac13ce87e80098adbf96dfe4b025b5184c
Karatsupa <andreykaratsupa@gmail.com>
2023-06-21 22:40:14 +0300

    Wrong

--- src/test/java/ru/netology/rest/PostmanTest.java
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -17,6 +17,6 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("some"));
     }
 }
